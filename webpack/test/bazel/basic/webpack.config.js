854183aa6240096dcc7b3399cb651120b5cddbbc
Paul Draper <paulddraper@gmail.com>
2021-12-11 21:29:11 -0700

    Remove unnecessary bind()

--- webpack/test/bazel/basic/webpack.config.js
+++ webpack/test/bazel/basic/webpack.config.js
@@ -2,6 +2,7 @@ const path = require("path");
 
 module.exports = {
   entry: path.resolve(`${process.env.WEBPACK_INPUT_ROOT}/main.js`),
+  mode: 'production',
   output: {
     filename: path.basename(process.env.WEBPACK_OUTPUT),
     path: path.resolve(path.dirname(process.env.WEBPACK_OUTPUT)),
