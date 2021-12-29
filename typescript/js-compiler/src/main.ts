47cbdb95d2e74ea76662b6b821746072d3ee37f5
Paul Draper <paulddraper@gmail.com>
2021-12-29 06:09:50 -0700

    Shim fs for TS compiler before loading other modules

--- typescript/js-compiler/src/main.ts
+++ typescript/js-compiler/src/main.ts
@@ -1,107 +1,20 @@
-import { PackageTree } from "@better-rules-javascript/commonjs-package";
-import { JsonFormat } from "@better-rules-javascript/commonjs-package/json";
 import { patchFs } from "@better-rules-javascript/nodejs-fs-linker/fs";
 import { patchFsPromises } from "@better-rules-javascript/nodejs-fs-linker/fs-promises";
-import { createVfs } from "@better-rules-javascript/nodejs-fs-linker/package";
 import { WrapperVfs } from "@better-rules-javascript/nodejs-fs-linker/vfs";
 import { run } from "@better-rules-javascript/worker";
-import { ArgumentParser } from "argparse";
-import * as fs from "fs";
-import * as path from "path";
-import * as ts from "typescript";
-import { formatDiagnostics } from "./diagnostic";
-
-interface Args {
-  config: string;
-  manifest: string;
-  map: string;
-  js: string;
-  src: string;
-}
-
-class WorkerError extends Error {}
-
-class WorkerArgumentParser extends ArgumentParser {
-  exit(status: number, message: string) {
-    throw new WorkerError(message);
-  }
-}
-
-class JsWorker {
-  constructor(private readonly vfs: WrapperVfs) {
-    this.parser.add_argument("--config", { required: true });
-    this.parser.add_argument("--manifest", { required: true });
-    this.parser.add_argument("--map", { required: true });
-    this.parser.add_argument("--js", { required: true });
-    this.parser.add_argument("src");
-  }
-
-  private readonly parser = new WorkerArgumentParser();
-
-  private parseConfig(config: string) {
-    const parsed = ts.getParsedCommandLineOfConfigFile(
-      config,
-      {},
-      {
-        ...ts.sys,
-        onUnRecoverableConfigFileDiagnostic: (error) => {
-          throw new WorkerError(formatDiagnostics([error]));
-        },
-      },
-    )!;
-    const errors = parsed.errors.filter(
-      (diagnostic) => diagnostic.code !== 18002,
-    );
-    if (errors.length) {
-      throw new WorkerError(formatDiagnostics(errors));
-    }
-    return parsed.options;
-  }
-
-  private setupVfs(manifest: string) {
-    const packageTree = JsonFormat.parse(
-      PackageTree.json(),
-      fs.readFileSync(manifest, "utf8"),
-    );
-    const vfs = createVfs(packageTree, false);
-    this.vfs.delegate = vfs;
-  }
-
-  run(a: string[]) {
-    const args: Args = this.parser.parse_args(a);
-
-    this.setupVfs(args.manifest);
-
-    const options = this.parseConfig(args.config);
-
-    const input = fs.readFileSync(args.src, "utf8");
-    const result = ts.transpileModule(input, {
-      fileName: args.src,
-      compilerOptions: options,
-    });
-    if (result.diagnostics.length) {
-      throw new WorkerError(formatDiagnostics(result.diagnostics));
-    }
-
-    fs.mkdirSync(path.dirname(args.js), { recursive: true });
-    fs.writeFileSync(args.js, result.outputText, "utf8");
-
-    fs.mkdirSync(path.dirname(args.map), { recursive: true });
-    fs.writeFileSync(args.map, result.sourceMapText, "utf8");
-  }
-}
 
 run(async () => {
   const vfs = new WrapperVfs();
   patchFs(vfs, require("fs"));
   patchFsPromises(vfs, require("fs").promises);
 
+  const { JsWorker, JsWorkerError } = await import("./worker");
   const worker = new JsWorker(vfs);
   return async (a) => {
     try {
       worker.run(a);
     } catch (e) {
-      if (e instanceof WorkerError) {
+      if (e instanceof JsWorkerError) {
         return { exitCode: 2, output: e.message };
       }
       return { exitCode: 1, output: String(e?.stack || e) };
---
+++ typescript/js-compiler/src/worker.ts
@@ -0,0 +1,89 @@
+import { PackageTree } from "@better-rules-javascript/commonjs-package";
+import { JsonFormat } from "@better-rules-javascript/commonjs-package/json";
+import { createVfs } from "@better-rules-javascript/nodejs-fs-linker/package";
+import { WrapperVfs } from "@better-rules-javascript/nodejs-fs-linker/vfs";
+import { ArgumentParser } from "argparse";
+import * as fs from "fs";
+import * as path from "path";
+import * as ts from "typescript";
+import { formatDiagnostics } from "./diagnostic";
+
+interface JsArgs {
+  config: string;
+  manifest: string;
+  map: string;
+  js: string;
+  src: string;
+}
+
+export class JsWorkerError extends Error {}
+
+class JsArgumentParser extends ArgumentParser {
+  exit(status: number, message: string) {
+    throw new JsWorkerError(message);
+  }
+}
+
+export class JsWorker {
+  constructor(private readonly vfs: WrapperVfs) {
+    this.parser.add_argument("--config", { required: true });
+    this.parser.add_argument("--manifest", { required: true });
+    this.parser.add_argument("--map", { required: true });
+    this.parser.add_argument("--js", { required: true });
+    this.parser.add_argument("src");
+  }
+
+  private readonly parser = new JsArgumentParser();
+
+  private parseConfig(config: string) {
+    const parsed = ts.getParsedCommandLineOfConfigFile(
+      config,
+      {},
+      {
+        ...ts.sys,
+        onUnRecoverableConfigFileDiagnostic: (error) => {
+          throw new JsWorkerError(formatDiagnostics([error]));
+        },
+      },
+    )!;
+    const errors = parsed.errors.filter(
+      (diagnostic) => diagnostic.code !== 18002,
+    );
+    if (errors.length) {
+      throw new JsWorkerError(formatDiagnostics(errors));
+    }
+    return parsed.options;
+  }
+
+  private setupVfs(manifest: string) {
+    const packageTree = JsonFormat.parse(
+      PackageTree.json(),
+      fs.readFileSync(manifest, "utf8"),
+    );
+    const vfs = createVfs(packageTree, false);
+    this.vfs.delegate = vfs;
+  }
+
+  run(a: string[]) {
+    const args: JsArgs = this.parser.parse_args(a);
+
+    this.setupVfs(args.manifest);
+
+    const options = this.parseConfig(args.config);
+
+    const input = fs.readFileSync(args.src, "utf8");
+    const result = ts.transpileModule(input, {
+      fileName: args.src,
+      compilerOptions: options,
+    });
+    if (result.diagnostics.length) {
+      throw new JsWorkerError(formatDiagnostics(result.diagnostics));
+    }
+
+    fs.mkdirSync(path.dirname(args.js), { recursive: true });
+    fs.writeFileSync(args.js, result.outputText, "utf8");
+
+    fs.mkdirSync(path.dirname(args.map), { recursive: true });
+    fs.writeFileSync(args.map, result.sourceMapText, "utf8");
+  }
+}
