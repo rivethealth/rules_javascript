93450a27cec84b2c0929f6430504b5c10632f644
Paul Draper <paulddraper@gmail.com>
2024-03-24 19:19:11 -0600

    Support overrides in Prettier configuration

--- prettier/format/src/index.ts
+++ prettier/format/src/index.ts
@@ -1,9 +1,5 @@
 import { workerMain } from "@better-rules-javascript/bazel-worker";
 import { ArgumentParser } from "argparse";
-import { dirname } from "node:path";
-import { pathToFileURL } from "node:url";
-import { Options } from "prettier";
-import { load, resolve } from "./import";
 
 interface Args {
   config?: string;
@@ -14,30 +10,8 @@ workerMain(async (a) => {
   parser.add_argument("--config", { help: "Configuration path" });
   const args: Args = parser.parse_args(a);
 
-  const { resolveConfig } = await import("prettier");
   const { PrettierWorker } = await import("./worker");
-
-  const options: Options | undefined =
-    args.config === undefined
-      ? undefined
-      : (await resolveConfig(args.config, { config: args.config })) ||
-        undefined;
-  if (options?.plugins) {
-    const contextUrl = pathToFileURL(dirname(args.config!));
-    options.plugins = await Promise.all(
-      options.plugins.map(async (plugin) => {
-        // in theory, should be able to just resolve the path, but for some reason
-        // that dereferences symlinks
-        if (typeof plugin === "string") {
-          const path = await resolve(plugin, contextUrl);
-          const p = await load(path);
-          plugin = p.default ?? p;
-        }
-        return plugin;
-      }),
-    );
-  }
-  const worker = new PrettierWorker(options);
+  const worker = new PrettierWorker(args.config);
 
   return async (a) => {
     try {
--- prettier/format/src/worker.ts
+++ prettier/format/src/worker.ts
@@ -1,20 +1,44 @@
 import { ArgumentParser } from "argparse";
 import { readFile, writeFile } from "node:fs/promises";
-import { Options, format } from "prettier";
+import { dirname } from "node:path";
+import { pathToFileURL } from "node:url";
+import { Options, format, resolveConfig } from "prettier";
+import { load, resolve } from "./import";
 
 export class PrettierWorker {
-  constructor(private readonly options: Options | undefined) {}
+  constructor(private readonly configPath: string | undefined) {}
+
+  async resolveConfig(path: string): Promise<Options | undefined> {
+    if (this.configPath === undefined) {
+      return;
+    }
+    const options = (await resolveConfig(path, { config: this.configPath }))!;
+    if (options.plugins) {
+      const contextUrl = pathToFileURL(dirname(this.configPath));
+      options.plugins = await Promise.all(
+        options.plugins.map(async (plugin) => {
+          // in theory, should be able to just resolve the path, but for some reason
+          // that dereferences symlinks
+          if (typeof plugin === "string") {
+            const path = await resolve(plugin, contextUrl);
+            const p = await load(path);
+            plugin = p.default ?? p;
+          }
+          return plugin;
+        }),
+      );
+    }
+    return options;
+  }
 
   async run(a: string[]) {
     const parser = new ArgumentParser();
     parser.add_argument("input");
     parser.add_argument("output");
     const args = parser.parse_args(a);
+    const options = await this.resolveConfig(args.input);
     const input = await readFile(args.input, "utf8");
-    const output = await format(input, {
-      ...this.options,
-      filepath: args.input,
-    });
+    const output = await format(input, { ...options, filepath: args.input });
     await writeFile(args.output, output, "utf8");
   }
 }
