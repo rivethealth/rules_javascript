a648fbe4ee132daafbe1349da46fdf63285931da
Paul Draper <paulddraper@gmail.com>
2025-02-02 01:16:39 -0700

    Test webpack with nested dependency

---
+++ webpack/test/bazel/nested-dep/index.js
@@ -0,0 +1,4 @@
+import { Foo } from "@better-rules-javascript-test/webpack-nested-dep-lib";
+
+const foo = new Foo("Example");
+foo.print();
---
+++ webpack/test/src/nested-dep.spec.ts
@@ -0,0 +1,11 @@
+import { bazelBin, spawnOptions } from "@better-rules-javascript/test";
+import { spawnSync } from "node:child_process";
+
+test("Build", async () => {
+  const result = spawnSync("bazel", ["build", "nested-dep:bundle"], {
+    cwd: "webpack/test/bazel",
+    stdio: "inherit",
+    ...spawnOptions(),
+  });
+  expect(result.status).toBe(0);
+});
