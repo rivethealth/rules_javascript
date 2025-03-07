8f94b0dfbc774f613bec0c4437bbbc6b7d1cbdd6
Nevada Perry <nevada.perry@rivethealth.com>
2025-03-07 01:20:38 +0000

    Try restricting Jest to .js and .json files

--- test/src/jest-config.ts
+++ test/src/jest-config.ts
@@ -1,5 +1,6 @@
 export function jestConfig() {
   return {
+    moduleFileExtensions: ["js", "json"],
     modulePathIgnorePatterns: [`${process.env.JEST_ROOT}/bazel/`],
     testPathIgnorePatterns: [`${process.env.JEST_ROOT}/bazel/`],
     testTimeout: 60 * 1000,
